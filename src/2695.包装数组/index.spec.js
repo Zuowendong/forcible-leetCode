fbd90efbbf7ffa118d0e0d1b2e75384ae13df34a
zwd <17326195691@163.com>
2024-01-24 10:14:44 +0800

    2695.包装数组

---
+++ src/2695.包装数组/index.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from "vitest";
+import { ArrayWrapper } from "./index";
+
+test("结果值为两个数组中所有元素的总和", () => {
+  const obj1 = new ArrayWrapper([1, 2]);
+  const obj2 = new ArrayWrapper([3, 4]);
+
+  expect(obj1 + obj2).toBe(10);
+});
+
+test("返回一个由逗号分隔的括在方括号中的字符串", () => {
+  const obj = new ArrayWrapper([23, 98, 42, 70]);
+  expect(obj.toString()).toBe("[23,98,42,70]");
+});
+
+test("空数组", () => {
+  const obj1 = new ArrayWrapper([]);
+  const obj2 = new ArrayWrapper([]);
+  expect(obj1 + obj2).toBe(0);
+});
