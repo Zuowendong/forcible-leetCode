610a1150dc7dd89d761faed10c66c1c8f8372596
zwd <17326195691@163.com>
2024-01-26 10:17:06 +0800

    2648.生成斐波那契数列

---
+++ src/2648.生成斐波那契数列/index.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from "vitest";
+
+import { fibGenerator } from "./index";
+
+test("callCount 5", () => {
+  const gen = fibGenerator();
+
+  expect(gen.next().value).toBe(0);
+  expect(gen.next().value).toBe(1);
+  expect(gen.next().value).toBe(1);
+  expect(gen.next().value).toBe(2);
+  expect(gen.next().value).toBe(3);
+  expect(gen.next().value).toBe(5);
+  expect(gen.next().value).toBe(8);
+  expect(gen.next().value).toBe(13);
+  expect(gen.next().value).toBe(21);
+});
